58b447ff773a2aa6617dc12461b751c876b2928f
eutinawei <46619191+etwyt@users.noreply.github.com>
2022-04-18 11:03:06 -0400

    add welcome typewriter page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import Typewriter from 'typewriter-effect'
+import sea from './assets/sea.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Wrapper = styled.div`
+  min-height: 100vh;
+  height: 100%;
+  margin: -8px;
+  font-family: "Courier";
+  background: url(${sea}) no-repeat center/cover;
+`
+
+const Header = styled.div`
+  position: absolute;
+  right: 20%;
+  top: 38%;
+  font-size: 50px;
+  text-align: right;
+  color: #ffffff;
+  text-shadow: 3px 3px rgba(0, 0, 0, 0.05);
+`
+
+const App = () => (
+  <Wrapper>
+    <Header>
+      <Typewriter
+        options={{
+          strings: "hello,<br>this is eutina wei,<br>an amateur photographer<br>based in pittsburgh.",
+          autoStart: true,
+        }}
+      />
+    </Header>
+  </Wrapper>
+)
 
 export default App;
